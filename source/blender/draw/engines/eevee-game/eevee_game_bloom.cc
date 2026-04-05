df5a92045278fd6bb85f368c9b62a3571ae0766a
Jorge Bernal <jbernalmartinez@gmail.com>
2026-04-05 18:49:43 +0200

    UPBGE: Fix eevee_game_bloom.hh include

--- source/blender/draw/engines/eevee-game/eevee_game_bloom.cc
+++ source/blender/draw/engines/eevee-game/eevee_game_bloom.cc
@@ -1,7 +1,7 @@
 /* SPDX-FileCopyrightText: 2023 Blender Authors
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "eevee_game_bloom.cc"
+#include "eevee_game_bloom.hh"
 #include "eevee_game_instance.hh"
 
 namespace blender::eevee_game {
